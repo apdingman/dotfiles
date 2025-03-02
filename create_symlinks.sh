3a4ce3aa9b3e4b3663a89fd957270b318de309a2
Adam Dingman <adam@adamdingman.net>
2025-03-02 15:36:54 -0500

    fix lns

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,3 +1,3 @@
 ln -sfn ~/src/dotfiles/.zshrc ~/.zshrc
-ln -sfn ~/src/dotfiles/nvim/ ~/.config/nvim/
-ln -sfn ~/src/dotfiles/ghostty/ ~/.config/ghostty/
+ln -sfn ~/src/dotfiles/nvim ~/.config/nvim
+ln -sfn ~/src/dotfiles/ghostty ~/.config/ghostty
